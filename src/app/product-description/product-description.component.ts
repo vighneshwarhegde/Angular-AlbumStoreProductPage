23a82624bdd2911cecdf11ef891263a4f161721e
Vighneshwar Hegde <v.hegde@sap.com>
2019-11-18 21:06:59 +0530

    interface1

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ProductService } from '../product.service';
-import { Album } from 'app/album';
+import { Album } from '../album';
 
 @Component({
   selector: 'app-product-description',
